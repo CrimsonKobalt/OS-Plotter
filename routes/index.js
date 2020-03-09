bd79064b52cba231350e7881d65db2ff8ad2a0e3
Christophe De Backer <christophe.db@live.be>
2020-03-09 16:19:00 +0100

    updated Home page

--- routes/index.js
+++ routes/index.js
@@ -14,32 +14,7 @@ router.get('/scheduling', function(req, res, next) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(req.query.dataset){
-    let xmlSize = req.query.dataset;
-
-    //    FCFS test block  -- working
-    //let FCFSinstance = FCFS.create(xmlSize);
-    //FCFSinstance.printAverages();
-    //FCFSinstance.sortOn_Service();
-    //let testarray = FCFSinstance.createDataset();
-    //console.log(testarray);
-
-    //    SJF test block  --  working
-    
-    //    SRT test block  --  working
-
-    //    RR test block -- working
-
-    //    HRRN test block -- working
-
-    //    MLFB test block -- under construction
-    let MLFBinstance = MLFB.create(xmlSize, "linear");
-    MLFBinstance.printAverages();
-
-    res.render('../views/index', { dataset: xmlSize });
-  } else {
   res.render('../views/index');
-  }
 });
 
 module.exports = router;
